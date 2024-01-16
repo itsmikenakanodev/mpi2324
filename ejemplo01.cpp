88f78dc01ad59a5d7086652b56a71276d1805188
Michael Garcia <mike.dev.programacion@gmail.com>
2024-01-16 13:44:23 +0000

    ejemplo 02 y ejemplo 03 comunicacion asincrona

--- ejemplo01.cpp
+++ ejemplo01.cpp
@@ -9,6 +9,16 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
+    if(rank == 0){
+         MPI_Send(&rank //referencia al vector de elementos a enviar
+    		,1 // tamaño del vector a enviar
+    		,MPI_INT // Tipo de dato que envias
+    		,rank+1 // pid del proceso destino
+    		,0 //etiqueta
+    		,MPI_COMM_WORLD); //Comunicador por el que se manda
+        
+    }
+
     std::printf("Rank %d of %d procs\n",rank,nprocs);
 
     MPI_Finalize();
